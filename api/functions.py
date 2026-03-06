a9fcb5032b7ec9adf52fcaaa8b7b8ff7b82e5f8b
mdevelopeur <mdevelopeur@gmail.com>
2026-03-06 15:55:12 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -27,12 +27,13 @@ async def get_products(client, last_id):
   }
   result = await client.post(url=url, headers=mpfit_headers, json=body)
   result = result.json()
-  
-  products = result["data"]
+  #products = result["data"]
   if len(result["data"]) == 200:
     products = await get_products(client, result["last_id"])
     products.extend(result["data"])
-  return products
+    return products
+  else:
+    return result["data"]
 
 def get_order_items(items, products):
   output = []
@@ -55,9 +56,13 @@ async def get_orders(client, id_list, last_id):
   body = {"limit": 200, "last_id": 0, "filter": {"ids": id_list[:200]}}
   result = await client.post(url=url, headers=mpfit_headers, json=body)
   result = result.json()
-  if len(result["data"]) == 200:
-    
-  return 
+  id_list = id_list[200:]
+  if id_list:
+    orders = await get_orders(client, id_list, result["last_id"])
+    orders.extend(result["data"])
+    return orders
+  else:
+    return result["data"]
 
 async def update_order(client, order, status):
   url = f"{insales}{order}.json"
