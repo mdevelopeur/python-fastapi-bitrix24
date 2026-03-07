579da3101f57d6d76d98dc4d1a15b583cbcd46d8
mdevelopeur <mdevelopeur@gmail.com>
2026-03-07 21:34:44 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 import multipart
 import re
+from api.functions import new_order_handler
 #from api.handlers import set_time, update, clear_keys, hash_password
 from urllib.parse import unquote, urlparse
 
@@ -21,7 +22,8 @@ async def post_handler(request: Request):
     try:
         body = await request.json()
         print(body)
-        return 
+        result = await new_order_handler(body)
+        return result
     except Exception as e:
         print(e)
         return e
