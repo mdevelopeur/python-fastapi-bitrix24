1f126aba4a5279beb3754920c4f9685bd87bd1be
mdevelopeur <mdevelopeur@gmail.com>
2026-03-13 16:05:56 +0500

    Update pochta_functions.py

--- api/pochta_functions.py
+++ api/pochta_functions.py
@@ -1,5 +1,14 @@
-
-
 load_dotenv(dotenv_path=".env.local")
 pochta_token = os.getenv("POCHTA_APP_AUTH_TOKEN")
 pochta_key = os.getenv("POCHTA_CLIENT_AUTH_KEY")
+pochta_headers = {
+  "X-User-Authorization": f"Basic {pochta_key}",
+  "Authorization": f"AccessToken {pochta_token}",
+  "Content-Type": "application/json;charset=UTF-8"
+}
+                  
+async def get_pochta_order_status(client, order):
+  url = f"https://otpravka-api.pochta.ru/1.0/backlog/search?query={order}"
+  result = await client.get(url=url, headers=pochta_headers)
+  result = result.json()
+  return result
