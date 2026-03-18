5125443d49c305f244d7ba0caa8705af67496fce
mdevelopeur <mdevelopeur@gmail.com>
2026-03-18 16:01:21 +0500

    Update pochta_functions.py

--- api/pochta_functions.py
+++ api/pochta_functions.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+import time
+import os
+
+
+
 load_dotenv(dotenv_path=".env.local")
 pochta_token = os.getenv("POCHTA_APP_AUTH_TOKEN")
 pochta_key = os.getenv("POCHTA_CLIENT_AUTH_KEY")
