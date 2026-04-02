c9ca8fdf85d992ba367fe783691e4917bc4ecdc6
mdevelopeur <mdevelopeur@gmail.com>
2026-04-02 17:27:36 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -12,108 +12,30 @@ import string
 import random 
 import hashlib
 from dotenv import load_dotenv
-from api.cdek_functions import get_cdek_tracking_number
-from api.pochta_functions import get_pochta_tracking_number
-
-mpfit = "https://app.mpfit.ru/api/v1/"
-insales = "https://myshop-dbn10.myinsales.ru/admin/orders/"
 
 load_dotenv(dotenv_path=".env.local")
 redis_url = os.getenv("REDIS_URL")
-mpfit_token = os.getenv("MPFIT_TOKEN")
-mpfit_headers = {"Authorization": f"Bearer {mpfit_token}", "Content-Type": "application/json"}
-insales_username = os.getenv("INSALES_USERNAME")
-insales_password = os.getenv("INSALES_PASSWORD")
-insales_auth = httpx.BasicAuth(username=insales_username, password=insales_password)
+bitrix24_url = os.getenv("BITRIX24_URL")
 
-statuses = {
-  "PRODUCTS_RESERVED": "v-obrabotke",
-  "EQUIPMENT": "v-obrabotke",
-  "READY_TO_SHIP": "soglasovan",
-  "SHIPPED": "dispatched",
-  "DELIVERY": "dispatched",
-  "COMPLETE": "dostavlen",
-  "REJECT": "vozvrat",
-  "CANCEL": "otmenen"
-}
-  
-async def new_order_handler(data):
+async def new_collab_handler(id):
   r = redis.Redis.from_url(redis_url, decode_responses=True)
   async with httpx.AsyncClient() as client:
-    products = await get_products(client, 0)
-    items = get_order_items(data["order_lines"], products)
-    note = f"""
-       Полное имя: {check(data["shipping_address"]["full_name"])};
-       Телефон: {check(data["shipping_address"]["phone"])};
-       Адрес: {check(data["shipping_address"]["full_delivery_address"])};
-       Подъезд: {check(data["shipping_address"]["entrance"])};
-       Этаж: {check(data["shipping_address"]["floor"])};
-       Домофон: {check(data["shipping_address"]["doorphone"])};
-       Способ доставки: {check (data["delivery_title"])}
-       Комментарий к доставке: {check(data["shipping_address"]["comment"])};
-       
-       Способ оплаты: {check(data["payment_title"])};
-       Комментарий покупателя: {check(data["comment"])};
-       Комментарий продавца: {check(data["manager_comment"])}
-    """
+    collab_data = await get_collab_data(client, id)
+    if collab_data["TYPE"] == "collab":
+      
+
     order_id = await create_order(client, items, note, data["id"])
     r.set(f"insales-mpfit:{order_id}", "NEW")
 
-async def get_products(client, last_id):
-  url = mpfit + "products/list"
-  body = {
-  "limit": 200,
-  "last_id": last_id
-  }
-  result = await client.post(url=url, headers=mpfit_headers, json=body)
-  result = result.json()
-  result = result["result"]
-  #products = result["data"]
-  if len(result["data"]) == 200:
-    products = await get_products(client, result["last_id"])
-    products.extend(result["data"])
-    return products
-  else:
-    return result["data"]
 
-def get_order_items(items, products):
-  output = []
-  for item in items:
-    product = next((product for product in products if product['article'] == item["sku"]), None)
-    if product is not None:
-      output.append({"product_id": product["id"], "quantity": item["quantity"]})
+async def get_collab_data(client, id):
+  url = bitrix24_url + "socialnetwork.api.workgroup.get"
+  body = {"params": {"groupId": id} }
+  response = await client.post(url, json=body)
+  response = response.json()
+  return response["result"]
 
-  return output
 
-async def create_order(client, items, note, id):
-  url = mpfit + "orders/create"
-  body = {"items": items, "shipment_date": "2024-04-20T09:20:06Z", "number": id, "note": note}
-  print(body)
-  result = await client.post(url=url, headers=mpfit_headers, json=body)
-  result = result.json()
-  return result["order"]["id"]
-
-async def get_orders(client, id_list, last_id):
-  url = mpfit + "orders/list"
-  body = {"limit": 200, "last_id": 0, "filter": {"ids": id_list[:200]}}
-  result = await client.post(url=url, headers=mpfit_headers, json=body)
-  result = result.json()
-  result = result["result"]
-  id_list = id_list[200:]
-  if id_list:
-    orders = await get_orders(client, id_list, result["last_id"])
-    orders.extend(result["data"])
-    return orders
-  else:
-    return result["data"]
-
-async def update_order(client, order, status):
-  url = f"{insales}{order}.json"
-  status = statuses[status]
-  body = {"order": {"custom_status_permalink": status}}
-  result = await client.put(url=url, auth=insales_auth, json=body)
-  result = result.json()
-  return 
 
 async def update_orders():
   r = redis.Redis.from_url(redis_url, decode_responses=True)
